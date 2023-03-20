9fdbdd49e748f38875ccff08d14116744a6488b2
turb0Code <turb000.Max.10@gmail.com>
2023-03-20 23:03:30 +0100

    Fully working basic sending and receiving.

--- client.py
+++ client.py
@@ -1,20 +1,34 @@
+"""
+pip install ahk
+pip install ahk[binary]
+pip install tinyec
+"""
+
 import threading
 import socket
 import sys
+import multiprocessing
+from ahk import AHK
+from ahk.window import Window
+
+##############################
 
 ADDRESS = "127.0.0.1"
 PORT = 9999
 
-current_action = ""
+##############################
 
-def recieve():
-    pass
+PROCESS = multiprocessing.current_process()
+AHK = AHK()
+#WINDOW = Window.from_pid(AHK, pid=str(PROCESS.pid))
+WINDOW = AHK.active_window
 
-def write():
-    pass
+##############################
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((ADDRESS, PORT))
+
+##############################
 
 if client.recv(1024).decode('utf-8') == "//NICKNAME":
     nickname = input("Enter your nickname -> ")
@@ -22,22 +36,41 @@ if client.recv(1024).decode('utf-8') == "//NICKNAME":
 
 print(f"{client.recv(1024).decode('utf-8')}")
 
-receive_thread = threading.Thread(target=recieve)
-write_thread = threading.Thread(target=write)
+def recieve_messages():
+    pass
+
+def write():
+    pass
+
+write_process = threading.Thread(target=write)
+recieve_messages = threading.Thread(target=recieve_messages)
 
 ##############################
 
-def recieve():
-    global write_thread
+def recieve_messages():
+    global nickname, WINDOW, AHK
+
     while True:
         try:
             recieved = client.recv(1024).decode("utf-8")
+            spaces = 11-len(recieved)
+            if 11-len(recieved) <= 0:
+                spaces = 0
 
             if recieved == "//NICKNAME":
                 pass
             else:
-                print(f"\033[F{recieved}\n")
-                write_thread.run()
+                if f"{nickname}: " in recieved:
+                    print(f"\033[F\033[K{recieved}" + ' '*spaces)
+                else:
+                    print(f"\033[F\033[K{recieved}" + ' '*spaces)
+                    if not WINDOW.exists:
+                        WINDOW.activate()
+                        AHK.type("\uCBA0\uCBBC\uCC9F\uCD95\uCDB7\uCE8C\uCF97\uCFA1\n")
+                        WINDOW.minimize()
+
+                    WINDOW.activate()
+                    AHK.type("\uCBA0\uCBBC\uCC9F\uCD95\uCDB7\uCE8C\uCF97\uCFA1\n")
         except:
             print("An ERROR occured!")
             client.close()
@@ -47,19 +80,24 @@ def recieve():
 
 def write():
     while True:
-        current_action = "WRITE"
-        print()
-        message = input(str("Message -> "))
-        client.send(f"{nickname}: {message}".encode('utf-8'))
+        message = '{}: {}'.format(nickname, input('Message -> \033[0;11'))
+
+        if "\uCBA0\uCBBC\uCC9F\uCD95\uCDB7\uCE8C\uCF97\uCFA1" not in message:
+            client.send(message.encode('utf-8'))
+
+        print('\r ', end='')
         if message == "//QUIT":
             return
 
+
 ##############################
 
-receive_thread = threading.Thread(target=recieve)
-write_thread = threading.Thread(target=write)
+if __name__ == "__main__":
 
-receive_thread.start()
-write_thread.start()
+    write_process = threading.Thread(target=write)
+    recieve_messages = threading.Thread(target=recieve_messages)
 
-sys.exit(0)
+    write_process.start()
+    recieve_messages.start()
+
+    sys.exit(0)
