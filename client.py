0bf52740f9467d5a1ab1ac94e3405630c274a5c1
turb0Code <turb000.Max.10@gmail.com>
2023-06-03 11:28:03 +0200

    0.03  Done messages encryption
    Update have introduced encryption feature for messages.

--- client.py
+++ client.py
@@ -10,6 +10,7 @@ import sys
 from ahk import AHK
 from ahk.window import Window
 from tinyec import registry
+from tinyec import ec
 import secrets
 
 ##############################
@@ -17,6 +18,7 @@ import secrets
 class Encryption:
 
     curve = registry.get_curve('brainpoolP256r1')
+    modulo = sys.maxunicode
 
     @staticmethod
     def create_pv_key():
@@ -25,6 +27,38 @@ class Encryption:
     @staticmethod
     def create_pub_key(pv_key):
         return pv_key * Encryption.curve.g
+
+    @staticmethod
+    def force_create_point(x, y):
+        return ec.Point(Encryption.curve, x, y)
+
+    @staticmethod
+    def encrypt(message, key):
+        return ''.join(chr((ord(c) + key.x) % Encryption.modulo) for c in message)
+
+    @staticmethod
+    def decrypt(message, key):
+        return ''.join(chr((ord(c) - key.x) % Encryption.modulo) for c in message)
+
+    @staticmethod
+    def send_exchange_keys(client, nickname):
+        ecc_pv = Encryption.create_pv_key()
+        ecc_pb = Encryption.create_pub_key(ecc_pv)
+        client.send(f"//KEY[{nickname}]->{ecc_pb.x},{ecc_pb.y}".encode('utf-8'))
+        ecc_user_pb = client.recv(1024).decode('utf-8').split("->")[1].split(",")
+        ecc_user_pb = Encryption.force_create_point(int(ecc_user_pb[0]), int(ecc_user_pb[1]))
+        ecc_shared = ecc_pv * ecc_user_pb
+        #print(f"X: {ecc_shared.x} \nY: {ecc_shared.y}\n")
+        return ecc_shared
+
+    @staticmethod
+    def recieve_exchange_keys(client, ecc_client_pb):
+        ecc_pv = Encryption.create_pv_key()
+        ecc_pb = Encryption.create_pub_key(ecc_pv)
+        client.send(f"//HAHA->{ecc_pb.x},{ecc_pb.y}".encode('utf-8'))
+        ecc_shared = ecc_client_pb * ecc_pv
+        #print(f"X: {ecc_shared.x} \nY: {ecc_shared.y}\n")
+        return ecc_shared
 
 ##############################
 
@@ -39,34 +73,56 @@ class Chat:
 
     @staticmethod
     def send_message(client):
+        global ex_msg, ecc_shared
+
         while True:
             message = '{}: {}'.format(nickname, input('Message -> \033[0;11'))
+            #message = '{}: {}'.format(nickname, input('Message -> \033[11C')) ---COMMENTED---
 
-            if "\uCBA0\uCBBC\uCC9F\uCD95\uCDB7\uCE8C\uCF97\uCFA1\n" not in message:
-                client.send(message.encode('utf-8'))
+            if ex_msg == 3:
+                ecc_shared = Encryption.send_exchange_keys(client, nickname)
+                ex_msg = 0
 
-            print('\r ', end='')
+            if '\uCBA0\uCBBC\uCC9F\uCD95\uCDB7\uCE8C\uCF97\uCFA1' in message:
+                print('\033[F\r', end='')
+                print('\033[K', end='')
+            else:
+                client.send(Encryption.encrypt(message, ecc_shared).encode('utf-8'))
+                #client.send(message.encode('utf-8'))
+                print('\033[K\r', end='')
+                ex_msg += 1
+
+            #print('\r', end='')
             if message == "//QUIT":
                 return
 
     @staticmethod
     def recieve_messages(client):
-        global nickname, WINDOW, AHK
+        global nickname, WINDOW, AHK, ex_msg, ecc_shared
 
         while True:
             try:
                 recieved = client.recv(1024).decode("utf-8")
-                spaces = 11-len(recieved)
-                if 11-len(recieved) <= 0:
-                    spaces = 0
 
-                if recieved == "//NICKNAME" or recieved == "//CHATROOM":
+                if "//KEY" in recieved and nickname not in recieved:
+                        ecc_client_pb = recieved.split("->")[1].split(",")
+                        ecc_client_pb = Encryption.force_create_point(int(ecc_client_pb[0]), int(ecc_client_pb[1]))
+                        ecc_shared = Encryption.recieve_exchange_keys(client, ecc_client_pb)
+                        ex_msg = 0
+                elif recieved == "//NICKNAME" or recieved == "//CHATROOM" or "//HAHA" in recieved or ("//KEY" in recieved and nickname in recieved):
                     pass
                 else:
+                    recieved = Encryption.decrypt(recieved, ecc_shared)
+
                     if f"{nickname}: " in recieved:
-                        print(f"\033[F\033[K{recieved}" + ' '*spaces)
+                        print(f"\033[F\033[K{recieved}" + ' '*spaces) #MAYBE \r
                     else:
-                        print(f"\033[F\033[K{recieved}" + ' '*spaces)
+                        spaces = 11-len(recieved)
+                        if 11-len(recieved) <= 0:
+                            spaces = 0
+                        print(f"\r\033[K{recieved}" + ' '*spaces)
+                        del recieved
+
                         if not Chat.WINDOW.exists:
                             Chat.WINDOW.activate()
                             Chat.AHK.type("\uCBA0\uCBBC\uCC9F\uCD95\uCDB7\uCE8C\uCF97\uCFA1\n")
@@ -74,6 +130,9 @@ class Chat:
 
                         Chat.WINDOW.activate()
                         Chat.AHK.type('\uCBA0\uCBBC\uCC9F\uCD95\uCDB7\uCE8C\uCF97\uCFA1\n')
+                        ex_msg += 1
+
+
             except:
                 print("An ERROR occured!")
                 client.close()
@@ -82,23 +141,43 @@ class Chat:
 
 ##############################
 
+class Interface:
+    pass
+
+##############################
+
 if __name__ == "__main__":
 
     ADDRESS = "127.0.0.1"
     PORT = 9999
 
+    ex_msg = 2 # ex_msg -> exchanged messages
+
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((ADDRESS, PORT))
 
-    if client.recv(1024).decode('utf-8') == "//NICKNAME":
+    ecc_pv = Encryption.create_pv_key()
+    ecc_pb = Encryption.create_pub_key(ecc_pv)
+
+    request = client.recv(1024).decode('utf8')
+
+    if "//KEY" in request:
+        ecc_server_pb = request.split("->")[1].split(",")
+        ecc_server_pb = Encryption.force_create_point(int(ecc_server_pb[0]), int(ecc_server_pb[1]))
+        client.send(f"{ecc_pb.x},{ecc_pb.y}".encode('utf-8'))
+
+        ecc_shared = ecc_server_pb * ecc_pv
+
+        print(f"X: {ecc_shared.x} \nY: {ecc_shared.y}")
+
+    if Encryption.decrypt(client.recv(1024).decode('utf8'), ecc_shared) == "//NICKNAME":
         nickname = input("Enter your nickname -> ")
-        client.send(nickname.encode('utf-8'))
+        client.send(Encryption.encrypt(nickname, ecc_shared).encode('utf-8'))
 
-    if client.recv(1024).decode('utf-8') == "//CHATROOM":
-        print("TĘPA DZIDA")
-        client.send("kurwa".encode('utf-8'))
+    if Encryption.decrypt(client.recv(1024).decode('utf-8') , ecc_shared) == "//CHATROOM":
+        client.send(Encryption.encrypt("kurwa", ecc_shared).encode('utf-8'))
 
-    print(f"{client.recv(1024).decode('utf-8')}")
+    print(f"\033[K{client.recv(1024).decode('utf-8')}")
 
     write_process = threading.Thread(target=Chat.send_message, args=(client,))
     recieve_messages = threading.Thread(target=Chat.recieve_messages, args=(client,))
--- server.py
+++ server.py
@@ -3,10 +3,13 @@ import socket
 import time
 from tinyec import registry
 import secrets
+from tinyec import ec
+import sys
 
 class Encryption:
 
     curve = registry.get_curve('brainpoolP256r1')
+    modulo = sys.maxunicode
 
     @staticmethod
     def create_pv_key():
@@ -15,6 +18,18 @@ class Encryption:
     @staticmethod
     def create_pub_key(pv_key):
         return pv_key * Encryption.curve.g
+
+    @staticmethod
+    def force_create_point(x, y):
+        return ec.Point(Encryption.curve, x, y)
+
+    @staticmethod
+    def encrypt(message, key):
+        return ''.join(chr((ord(c) + key.x) % Encryption.modulo) for c in message)
+
+    @staticmethod
+    def decrypt(message, key):
+        return ''.join(chr((ord(c) - key.x) % Encryption.modulo) for c in message)
 
 class Background:
     pass
@@ -37,12 +52,22 @@ class Client():
 
             client, addr = server.accept()
 
-            client.send(f"//NICKNAME".encode('utf-8'))
-            nickname = client.recv(1024).decode('utf-8')
+            ecc_pv = Encryption.create_pv_key()
+            ecc_pb = Encryption.create_pub_key(ecc_pv)
+
+            client.send(f"//KEY->{ecc_pb.x},{ecc_pb.y}".encode('utf-8'))
+            ecc_user_pb = client.recv(1024).decode('utf-8').split(",")
+            ecc_user_pb = Encryption.force_create_point(int(ecc_user_pb[0]), int(ecc_user_pb[1]))
+
+            ecc_shared =  ecc_user_pb * ecc_pv
+            print(f"X: {ecc_shared.x} \nY: {ecc_shared.y}")
+
+            client.send(Encryption.encrypt("//NICKNAME", ecc_shared).encode('utf-8'))
+            nickname = Encryption.decrypt(client.recv(1024).decode('utf-8'), ecc_shared)
             nicknames.append(nickname)
 
-            client.send(f"//CHATROOM".encode('utf-8'))
-            chat_id = client.recv(1024).decode('utf-8')
+            client.send(Encryption.encrypt("//CHATROOM", ecc_shared).encode('utf-8'))
+            chat_id = Encryption.decrypt(client.recv(1024).decode('utf-8'), ecc_shared)
             new_client = Client(client, addr, nickname, chat_id)
 
             print(f"Connected with {str(addr)} as {nickname} to chat {new_client.chat_id}.")
@@ -50,19 +75,14 @@ class Client():
             rooms_ids = [room.id for room in rooms]
 
             if new_client.chat_id in rooms_ids:
-                print("SAME ROOM")
                 room = rooms[rooms_ids.index(new_client.chat_id)]
                 room.add_user(new_client)
-                #room_id_index = rooms_ids.index(new_client.chat_id)#  TĘPA  KURWA  #
+                #room_id_index = rooms_ids.index(new_client.chat_id) ---COMMENTED---
                 room_id_index = 0
             else:
-                print("NEW ROOM")
                 rooms.append(Room(new_client.chat_id))
                 room_id_index = -1
                 rooms[room_id_index].add_user(new_client)
-                print(rooms[room_id_index])
-
-            print(f"SZMACIARZU TU SĄ USERZY: {rooms[0].clients}")
 
             Chat.broadcast(f"{nickname} JOINED CHAT".encode('utf-8'), room_id_index)
 
@@ -101,9 +121,7 @@ class Chat:
 
     @staticmethod
     def broadcast(message, chat_id_index):
-        print(message)
-        print(len(rooms))
-
+        print(f'MSG: {message}')
         for client in rooms[chat_id_index].clients:
             client.client.send(message)
 
@@ -119,7 +137,6 @@ class Chat:
 
             #chat_id_index = [room.id for room in rooms].index(client.chat_id) ---COMMENTED---
             chat_id_index = 0
-            print(chat_id_index)
 
             try:
                 recieved = client.client.recv(1024)
